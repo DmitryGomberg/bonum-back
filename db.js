fb73cb43f56018e6a2490605faf7fb7387df2ed3
dmitry <dpgomberg@gmail.com>
2025-07-17 13:02:03 +0300

    lads

--- db.js
+++ db.js
@@ -12,11 +12,10 @@ requiredEnvVars.forEach((varName) => {
 });
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 module.exports = pool;
