6e66ebe37468217b7f1ddf18164f39f30083253d
dmitry <dpgomberg@gmail.com>
2025-07-17 13:25:00 +0300

    fix bag

--- db.js
+++ db.js
@@ -3,14 +3,6 @@ dotenv.config();
 
 const Pool = require('pg').Pool;
 
-const requiredEnvVars = ['DB_USER', 'DB_PASSWORD', 'DB_HOST', 'DB_PORT', 'DB_NAME'];
-
-requiredEnvVars.forEach((varName) => {
-    if (!process.env[varName]) {
-        throw new Error(`Environment variable ${varName} is not set`);
-    }
-});
-
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
